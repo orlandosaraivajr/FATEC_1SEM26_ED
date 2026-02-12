73b4d205c22202e6255216c89089f92651784fa4
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2026-02-12 11:26:58 -0300

    Update dojo.py

--- dojo/dojo.py
+++ dojo/dojo.py
@@ -1,10 +1,6 @@
 def numero_palindromo(numero):
-    # cria um texto baseado no numero
     numero_str = str(numero)
-    # inverte o texto
-    numero_invertido = numero_str[::-1]
-    # compara o texto original com o texto invertido (o palindromo)
-    return numero_str == numero_invertido
+    return numero_str == numero_str[::-1]
 
 assert numero_palindromo(121) == True
 assert numero_palindromo(-121) == False
